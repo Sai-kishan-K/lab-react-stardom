2a23fbb0dfb036b27eebc9fbdcdcb5e47f578dd6
Sai-kishan-K <saik77556@gmail.com>
2021-10-31 12:25:04 +0530

    Updated

---
+++ prostars/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Head from './components/Head';
+import Prostars from './components/Prostars';
+
+function App(){
+  return (
+    <div className = 'App'>
+      <Head/>
+      <Prostars/>
+    </div>
+  );
+}
+
+export default App;
